b1b35714004e5fdc83174148918125025938bac9
Diogo Ferreira <diogo.mv.ferreira19@outlook.pt>
2021-12-14 19:06:58 +0000

    Create pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    public FpsController fpsController;
+    private PlayerInput playerInput;
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    private void Start()
+    {
+       playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        if (playerInput.actions["Pause"].triggered)
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        fpsController.enabled = true;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        fpsController.enabled = false;
+        pauseMenuUI.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+}
